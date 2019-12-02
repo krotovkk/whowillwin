ba1eb98ff715ab2b2534bee3b512e02c38c948e2
Kirill Krotov <krotovkiko@gmail.com>
2019-12-02 11:07:33 +0300

    Лиги добавлены в навигационную панель

--- src/app/leagues-nav/leagues-nav.component.ts
+++ src/app/leagues-nav/leagues-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ILeagueCard } from "../model/ileaguecard";
+
 @Component({
   selector: 'app-leagues-nav',
   templateUrl: './leagues-nav.component.html',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaguesNavComponent implements OnInit {
 
+  leagues: ILeagueCard[]
+
   constructor() { }
 
   ngOnInit() {
+    this.leagues = this.getLeagues();
+  }
+
+  private getLeagues(): ILeagueCard[] {
+    return [
+      {
+        name: 'Англия',
+        value: 'england',
+        logoLink: 'https://media.api-football.com/leagues/2.png'
+      },
+      {
+        name: 'Германия',
+        value: 'germany',
+        logoLink: 'https://media.api-football.com/leagues/8.png'
+      },
+      {
+        name: 'Испания',
+        value: 'spain',
+        logoLink: 'https://media.api-football.com/leagues/87.png'
+      },
+      {
+        name: 'Италия',
+        value: 'italy',
+        logoLink: 'https://media.api-football.com/leagues/94.png'
+      },
+      {
+        name: 'Франция',
+        value: 'france',
+        logoLink: 'https://media.api-football.com/leagues/4.svg'
+      },
+      {
+        name: 'Россия',
+        value: 'russia',
+        logoLink: 'https://media.api-football.com/leagues/135.png'
+      },
+    ]
   }
 
 }
---
+++ src/app/model/ileaguecard.ts
@@ -0,0 +1,5 @@
+export interface ILeagueCard {
+  name: string,
+  value: string,
+  logoLink: string
+}
