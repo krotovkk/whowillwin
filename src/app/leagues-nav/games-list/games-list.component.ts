fd9a0a0c3e3ce6fefd248a68d6f85bf7180cbdd3
Kirill Krotov <krotovkiko@gmail.com>
2019-12-02 14:52:27 +0300

    Добавлен запрос к API

--- src/app/leagues-nav/games-list/games-list.component.ts
+++ src/app/leagues-nav/games-list/games-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IGame } from 'src/app/model/igame';
 
 @Component({
   selector: 'app-games-list',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games-list.component.css']
 })
 export class GamesListComponent implements OnInit {
+  games: IGame[];
+  league_id = 775;
+  round = 'Regular_Season_-_16';
 
   constructor() { }
 
   ngOnInit() {
+    this.getGames();
+  }
+
+  private async getGames() {
+    const data = await fetch("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/775/Regular_Season_-_16", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+        "x-rapidapi-key": "6b329fe810msh8c2e124ed165b44p129ec0jsn4c1f4247bc48"
+      }
+    }).then(r => r.json());
+    console.log(data);
+    
   }
 
 }
---
+++ src/app/model/igame.ts
@@ -0,0 +1,8 @@
+import { ITeam } from './iteam';
+
+export interface IGame {
+  homeTeam: ITeam,
+  awayTeam: ITeam,
+  goalsHomeTeam: number,
+  goalsAwayTeam: number,
+}
