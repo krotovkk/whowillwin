e492c011542f2f24e727f7d8c01a5a8df7fb96e6
Kirill Krotov <krotovkiko@gmail.com>
2019-12-11 13:03:11 +0300

    Добавлен роутинг для компонента с лигами

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HistoryComponent } from './history/history.component';
+import { LeaguesNavComponent } from './leagues-nav/leagues-nav.component';
 
 const appRoutes: Routes = [
   {
     path: 'history',
     component: HistoryComponent
+  },
+  {
+    path: 'games',
+    component: LeaguesNavComponent
   }
 ];
 
