ab5d69b9a52ad652a9e820cf1c54a1128a51da65
Kirill Krotov <krotovkiko@gmail.com>
2019-12-12 22:42:16 +0300

    Добавлена начальная верстка для истории прогнозов

--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistoryComponent } from './history.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [HistoryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
     HistoryComponent
