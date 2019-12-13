75b7a7adeaa806d9acafc8b1da2f7b30fe5f1402
Kirill Krotov <krotovkiko@gmail.com>
2019-12-13 17:33:15 +0300

    Добавлен редирект для непонятных путей

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HistoryComponent } from './history/history.component';
 import { LoginComponent } from './login/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LeaguesNavComponent } from './leagues-nav/leagues-nav.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
 const appRoutes: Routes = [
   {
@@ -14,6 +16,15 @@ const appRoutes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path:'',
+    component: LeaguesNavComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
   }
 ];
 
